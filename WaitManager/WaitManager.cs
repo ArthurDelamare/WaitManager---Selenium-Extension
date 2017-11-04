b3c9f9866e1f6d5e68e5763b1a870bb21ddcae66
Unknown <arthur.delamare1@viacesi.fr>
2017-11-04 17:59:07 +0100

    Initialisation Projet

---
+++ WaitManager/WaitManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace WaitManager
+{
+    public sealed class WaitManager
+    {
+        private IWebDriver Driver;
+        private WebDriverWait Wait;
+        private IWebElement Element;
+        private int waitingTime = 0;
+
+        public int WaitingTime
+        {
+            get
+            {
+                return waitingTime;
+            }
+            set
+            {
+                if (value >= 0)
+                    waitingTime = value;
+            }
+        }
+
+        public WaitManager(IWebDriver driver)
+        {
+            this.Driver = driver;
+            this.WaitingTime = 5;
+            this.Wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(this.WaitingTime));
+        }
+
+        public void ElementIsVisible(By locator)
+        {
+            Element = Wait.Until(ExpectedConditions.ElementIsVisible(locator));
+        }
+
+        public void ElementIsClickable(By locator)
+        {
+            Element = Wait.Until(ExpectedConditions.ElementToBeClickable(locator));
+        }
+    }
+}
